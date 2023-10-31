83665a533df12d785af3d198fc28011e1bc97a10
sandro <contact@sanqro.xyz>
2023-10-31 09:22:25 +0100

    add deta deployment

--- server/dist/index.js
+++ server/dist/index.js
@@ -2,18 +2,18 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
-const app = (0, express_1.default)();
-app.use(express_1.default.json());
-app.use((0, cors_1.default)());
-app.disable("etag");
-app.get("/", (req, res) => res.status(200).json({
-    msg: "This is the API of the following repository on GitHub: https://github.com/sanqro/IDPA_Hauptprojekt"
-}));
-const port = parseInt(process.env.PORT) || 8080;
-app.listen(port, () => {
-    console.log(`listening on port ${port}`);
+exports.__esModule = true;
+var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var app = (0, express_1["default"])();
+app.use((0, cors_1["default"])());
+app.get("/", function (req, res) {
+    return res.status(200).json({
+        msg: "Root endpoint of: https://github.com/sanqro/IDPA_Hauptprojekt"
+    });
+});
+var port = parseInt(process.env.PORT) || 8080;
+app.listen(port, function () {
+    console.log("listening on port ".concat(port));
 });
 module.exports = app;
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,18 +2,18 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-app.use(express.json());
+
 app.use(cors());
-app.disable("etag");
 
 app.get("/", (req, res) =>
   res.status(200).json({
-    msg: "This is the API of the following repository on GitHub: https://github.com/sanqro/IDPA_Hauptprojekt"
+    msg: "Root endpoint of: https://github.com/sanqro/IDPA_Hauptprojekt"
   })
 );
 
 const port = parseInt(process.env.PORT) || 8080;
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`listening on port ${port}`);
 });
 
