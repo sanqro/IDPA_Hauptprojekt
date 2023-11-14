7c9b0db8c6cc9c141582cdb770b4889373b3b74c
sandro <contact@sanqro.xyz>
2023-11-14 07:37:44 +0100

    disable etag

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,9 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-
+app.use(express.json());
 app.use(cors());
+app.disable("etag");
 
 app.get("/", (req, res) =>
   res.status(200).json({
