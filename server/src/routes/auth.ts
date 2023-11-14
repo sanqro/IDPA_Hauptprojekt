29179a8ad8825e844a59aea49bbe1412c4e77973
sandro <contact@sanqro.xyz>
2023-11-14 08:12:55 +0100

    add registration endpoint

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -2,16 +2,41 @@ import express from "express";
 import * as dotenv from "dotenv";
 import path from "path";
 import { Deta } from "deta";
+import { IAuthFormData } from "../interfaces/interfaces";
+import argon2 from "argon2";
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 // deta setup
 const projectKey: string = process.env.DETA_PROJECT_KEY;
 const deta = Deta(projectKey);
-const auth = deta.Base("users");
+const users = deta.Base("users");
 
 const router = express.Router();
 
-router.post("/register", async (req, res) => {});
+router.post("/register", async (req, res) => {
+    try {
+        const authFormData: IAuthFormData = req.body as IAuthFormData;
+    
+        if (!(await users.get(authFormData.username))) {
+          const passwordHash = await argon2.hash(authFormData.password);
+          const auhtFormDataJson = {
+            key: authFormData.username,
+            password: passwordHash
+          };
+
+          const userToCreate = await users.insert(auhtFormDataJson);
+    
+          res.status(201).json({
+            username: userToCreate.key,
+            success: true
+          });
+        } else {
+          throw new Error("This username is already taken");
+        }
+      } catch (err) {
+        res.status(503).json({ error: err.message });
+      }
+});
 
 router.post("/login", async (req, res) => {});
 
