682af57a1d92404fbd60b73cc6a27195bcfed7d5
sandro <contact@sanqro.xyz>
2023-11-14 08:31:48 +0100

    add login endpoint

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -4,6 +4,7 @@ import path from "path";
 import { Deta } from "deta";
 import { IAuthFormData } from "../interfaces/interfaces";
 import argon2 from "argon2";
+import jwt from "jsonwebtoken";
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 // deta setup
@@ -11,12 +12,14 @@ const projectKey: string = process.env.DETA_PROJECT_KEY;
 const deta = Deta(projectKey);
 const users = deta.Base("users");
 
+// jwt setup
+const jwtSecret: string = process.env.JWT_SECRET;
+
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
     try {
         const authFormData: IAuthFormData = req.body as IAuthFormData;
-    
         if (!(await users.get(authFormData.username))) {
           const passwordHash = await argon2.hash(authFormData.password);
           const auhtFormDataJson = {
@@ -38,6 +41,34 @@ router.post("/register", async (req, res) => {
       }
 });
 
-router.post("/login", async (req, res) => {});
+router.post("/login", async (req, res) => {
+    try {
+        const authFormData: IAuthFormData = req.body as IAuthFormData;
+    
+        const user = await users.get(authFormData.username);
+    
+        if (user === null) {
+            res.status(401).json({
+                error: "Wrong credentials! Please try again.",
+                success: false
+              });
+              return 0;
+        }
+    
+        const password = user.password as string;
+    
+        if (await argon2.verify(password, authFormData.password)) {
+          const token = jwt.sign({ username: user.key }, jwtSecret, { expiresIn: "21600s" });
+          res.status(200).json({ token, success: true });
+        } else {
+          res.status(401).json({
+            error: "Wrong credentials! Please try again.",
+            success: false
+          });
+        }
+      } catch (err) {
+        res.status(503).json({ error: err.message });
+      }
+});
 
 export default router;
