fe21e7daa688aa4c256b1e5684a093479dd50c97
sandro <contact@sanqro.xyz>
2023-11-14 08:53:02 +0100

    add create endpoint for sets

--- server/src/routes/sets.ts
+++ server/src/routes/sets.ts
@@ -2,19 +2,47 @@ import express from "express";
 import * as dotenv from "dotenv";
 import path from "path";
 import { Deta } from "deta";
+import { ISetData } from "../interfaces/interfaces";
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 // deta setup
 const projectKey: string = process.env.DETA_PROJECT_KEY;
 const deta = Deta(projectKey);
-const auth = deta.Base("sets");
+const sets = deta.Base("sets");
 
 const router = express.Router();
 
-router.post("/update/:id", async (req, res) => {
-});
-
 router.post("/create", async (req, res) => {
+    try {
+        const setData: ISetData = req.body as ISetData;
+    
+        if (await sets.get(setData.title + setData.creator)) {
+            throw new Error("This set exists already. Please use a different title.");
+        }
+    
+        const setDataJson = {
+          key: setData.title + setData.creator,
+          title: setData.title,
+          creator: setData.creator,
+          question: setData.question,
+          answer: setData.answer
+        };
+    
+        const newSet = await sets.insert(setDataJson);
+    
+        res.status(201).json({
+          title: setData.title,
+          creator: setData.creator,
+          question: setData.question,
+          answer: setData.answer,
+          success: true
+        });
+      } catch (err) {
+        res.status(503).json({ error: err.message });
+      }
+    });
+
+router.post("/update/:id", async (req, res) => {
 });
 
 router.delete("/delete/:id", async (req, res) => {
