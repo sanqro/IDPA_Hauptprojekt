c790dffedfb54c51add5212d3896976380a614fb
sandro <contact@sanqro.xyz>
2023-12-12 07:50:46 +0100

    add endpoint to update set

--- server/src/routes/sets.ts
+++ server/src/routes/sets.ts
@@ -13,71 +13,95 @@ const sets = deta.Base("sets");
 const router = express.Router();
 
 router.post("/create", async (req, res) => {
-    try {
-        const setData: ISetData = req.body as ISetData;
-    
-        if (await sets.get(setData.title + setData.creator)) {
-            throw new Error("This set exists already. Please use a different title.");
-        }
-    
-        const setDataJson = {
-          key: setData.title + setData.creator,
-          title: setData.title,
-          creator: setData.creator,
-          question: setData.question,
-          answer: setData.answer
-        };
-    
-        const newSet = await sets.insert(setDataJson);
-    
-        res.status(201).json({
-          title: setData.title,
-          creator: setData.creator,
-          question: setData.question,
-          answer: setData.answer,
-          success: true
-        });
-      } catch (err) {
-        res.status(503).json({ error: err.message });
-      }
+  try {
+    const setData: ISetData = req.body as ISetData;
+
+    if (await sets.get(setData.title + setData.creator)) {
+      throw new Error("This set exists already. Please use a different title.");
+    }
+
+    const setDataJson = {
+      key: setData.title + setData.creator,
+      title: setData.title,
+      creator: setData.creator,
+      question: setData.question,
+      answer: setData.answer
+    };
+
+    await sets.insert(setDataJson);
+
+    res.status(201).json({
+      title: setData.title,
+      creator: setData.creator,
+      question: setData.question,
+      answer: setData.answer,
+      success: true
     });
-
-router.post("/update/:id", async (req, res) => {
+  } catch (err) {
+    res.status(503).json({ error: err.message });
+  }
 });
 
-router.delete("/delete/:id", async (req, res) => {
+router.post("/update", async (req, res) => {
+  try {
+    const setData: ISetData = req.body as ISetData;
+
+    if (!(await sets.get(req.body.oldKey))) {
+      throw new Error("This set does not exist.");
+    }
+
+    const setDataJson = {
+      key: setData.title + setData.creator,
+      title: setData.title,
+      creator: setData.creator,
+      answer: setData.answer,
+      question: setData.question
+    };
+    await sets.delete(req.body.oldKey);
+    await sets.insert(setDataJson);
+
+    res.status(201).json({
+      title: setDataJson.title,
+      creator: setDataJson.creator,
+      question: setDataJson.question,
+      answer: setDataJson.answer,
+      success: true
+    });
+  } catch (err) {
+    res.status(503).json({ error: err.message });
+  }
 });
+
+router.delete("/delete/:id", async (req, res) => {});
 
 router.get("/getAll", async (req, res) => {
-    try {
-        const fetchedSets = await sets.fetch();
-        if (fetchedSets  === null) {
-            throw new Error("No sets found");
-        }
-        res.status(201).json({ fetchedSets });
-      } catch (err) {
-        res.status(503).json({ error: err.message });
-      }
-    });
-
-router.get("/get/:id", async (req, res) => {
-    try {
-        const fetchedSet = await sets.get(req.params.id);
-        if (fetchedSet == null) {
-          res.status(409).json({
-            error: "This set does not exist.",
-          });
-          return false;
-        } else {
-          res.status(201).json({ fetchedSet });
-        }
-      } catch (err) {
-        res.status(503).json({ error: err.message });
-      }
-    });
-
-router.get("/get/:number", async (req, res) => {
+  try {
+    const fetchedSets = await sets.fetch();
+    if (fetchedSets === null) {
+      throw new Error("No sets found");
+    }
+    res.status(201).json({ fetchedSets });
+  } catch (err) {
+    res.status(503).json({ error: err.message });
+  }
 });
 
+router.get("/get/:id", async (req, res) => {
+  try {
+    const fetchedSet = await sets.get(req.params.id);
+    if (fetchedSet == null) {
+      res.status(409).json({
+        error: "This set does not exist."
+      });
+      return false;
+    } else {
+      res.status(201).json({ fetchedSet });
+    }
+  } catch (err) {
+    res.status(503).json({ error: err.message });
+  }
+});
+
+router.get("/get/:number", async (req, res) => {});
 
 export default router;
