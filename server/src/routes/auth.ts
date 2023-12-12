19006f0a69b0d3a9f21c930eae0a20fda5e9cf0c
sandro <contact@sanqro.xyz>
2023-12-12 07:44:06 +0100

    fix erors in auth route

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -5,79 +5,85 @@ import { Deta } from "deta";
 import { IAuthFormData } from "../interfaces/interfaces";
 import argon2 from "argon2";
 import jwt from "jsonwebtoken";
+
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
-// deta setup
+// Deta setup
 const projectKey: string = process.env.DETA_PROJECT_KEY;
 const deta = Deta(projectKey);
 const users = deta.Base("users");
 
-// jwt setup
+// JWT setup
 const jwtSecret: string = process.env.JWT_SECRET;
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-    try {
-        const authFormData: IAuthFormData = req.body as IAuthFormData;
+  try {
+    const authFormData: IAuthFormData = req.body as IAuthFormData;
 
-        if (authFormData.username === "" || authFormData.password === "" || authFormData.username === undefined || authFormData.password === undefined) {
-            throw new Error("Invalid Request");
-          }
+    if (authFormData.username === "" || authFormData.password === "") {
+      throw new Error("Invalid Request");
+    }
 
-        if (!(await users.get(authFormData.username))) {
-          const passwordHash = await argon2.hash(authFormData.password);
-          const auhtFormDataJson = {
-            key: authFormData.username,
-            password: passwordHash
-          };
+    if (!(await users.get(authFormData.username))) {
+      const passwordHash = await argon2.hash(authFormData.password);
+      const authFormDataJson = {
+        key: authFormData.username,
+        password: passwordHash
+      };
 
-          const userToCreate = await users.insert(auhtFormDataJson);
-    
-          res.status(201).json({
-            username: userToCreate.key,
-            success: true
-          });
-        } else {
-          throw new Error("This username is already taken");
-        }
-      } catch (err) {
-        res.status(503).json({ error: err.message });
-      }
+      const userToCreate = await users.insert(authFormDataJson);
+
+      res.status(201).json({
+        username: userToCreate.key,
+        success: true
+      });
+    } else {
+      throw new Error("This username is already taken");
+    }
+  } catch (err) {
+    res.status(503).json({ error: err.message });
+  }
 });
 
 router.post("/login", async (req, res) => {
-    try {
-        const authFormData: IAuthFormData = req.body as IAuthFormData;
-    
-        if (authFormData.username === "" || authFormData.password === "" || authFormData.username === undefined || authFormData.password === undefined) {
-            throw new Error("Invalid Request");
-          }
-          
-        const user = await users.get(authFormData.username);
-    
-        if (user === null) {
-            res.status(401).json({
-                error: "Wrong credentials! Please try again.",
-                success: false
-              });
-              return 0;
-        }
-    
-        const password = user.password as string;
-    
-        if (await argon2.verify(password, authFormData.password)) {
-          const token = jwt.sign({ username: user.key }, jwtSecret, { expiresIn: "21600s" });
-          res.status(200).json({ token, success: true });
-        } else {
-          res.status(401).json({
-            error: "Wrong credentials! Please try again.",
-            success: false
-          });
-        }
-      } catch (err) {
-        res.status(503).json({ error: err.message });
-      }
+  try {
+    const authFormData: IAuthFormData = req.body as IAuthFormData;
+
+    if (
+      authFormData.username === "" ||
+      authFormData.password === "" ||
+      authFormData.username === undefined ||
+      authFormData.password === undefined
+    ) {
+      throw new Error("Invalid Request");
+    }
+
+    const user = await users.get(authFormData.username);
+
+    if (!user) {
+      res.status(401).json({
+        error: "Wrong credentials! Please try again.",
+        success: false
+      });
+      return;
+    }
+
+    const password = user.password as string;
+
+    if (await argon2.verify(password, authFormData.password)) {
+      const token = jwt.sign({ username: user.key }, jwtSecret, { expiresIn: "21600s" });
+      res.status(200).json({ token, success: true });
+    } else {
+      res.status(401).json({
+        error: "Wrong credentials! Please try again.",
+        success: false
+      });
+    }
+  } catch (err) {
+    res.status(503).json({ error: err.message });
+  }
 });
 
 export default router;
