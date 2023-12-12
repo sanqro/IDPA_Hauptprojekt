d4712489a9790c6154c4912bfe943d3233007868
sandro <contact@sanqro.xyz>
2023-12-12 08:09:23 +0100

    add endpoint to create score

--- server/src/routes/scores.ts
+++ server/src/routes/scores.ts
@@ -2,6 +2,7 @@ import express from "express";
 import * as dotenv from "dotenv";
 import path from "path";
 import { Deta } from "deta";
+import { IScoreData } from "../interfaces/interfaces";
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 // deta setup
@@ -12,20 +13,31 @@ const scores = deta.Base("scores");
 const router = express.Router();
 
 router.post("/create", async (req, res) => {
+  try {
+    const scoreData: IScoreData = req.body as IScoreData;
+
+    if (await scores.get(scoreData.username + scoreData.set)) {
+      throw new Error("This score exists already. Please use a different name.");
+    }
+
+    const scoreDataJson = {
+      key: scoreData.username + scoreData.set,
+      username: scoreData.username,
+      set: scoreData.set,
+      score: scoreData.score
+    };
+
+    await scores.insert(scoreDataJson);
+
+    res.status(201).json({
+      username: scoreData.username,
+      set: scoreData.set,
+      score: scoreData.score,
+      success: true
+    });
+  } catch (err) {
+    res.status(503).json({ error: err.message });
+  }
 });
-
-router.post("/update/:id", async (req, res) => {
-});
-
-router.delete("/delete/:id", async (req, res) => {
-});
-
-router.get("/getAll", async (req, res) => {
-});
-
-router.get("/get/:id", async (req, res) => {
-});
-
-
 
 export default router;
