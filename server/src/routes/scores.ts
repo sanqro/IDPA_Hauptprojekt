e97e4871b89760fb103cf7fd40cd8929bb1e9ada
sandro <contact@sanqro.xyz>
2023-12-12 08:11:36 +0100

    add endpoint to update score

--- server/src/routes/scores.ts
+++ server/src/routes/scores.ts
@@ -40,4 +40,33 @@ router.post("/create", async (req, res) => {
   }
 });
 
+router.post("/update", async (req, res) => {
+  try {
+    const scoreData: IScoreData = req.body as IScoreData;
+
+    if (!(await scores.get(req.body.oldKey))) {
+      throw new Error("This set does not exist.");
+    }
+
+    const scoreDataJson = {
+      key: scoreData.username + scoreData.set,
+      username: scoreData.username,
+      set: scoreData.set,
+      score: scoreData.score
+    };
+
+    await scores.delete(req.body.oldKey);
+    await scores.insert(scoreDataJson);
+
+    res.status(201).json({
+      username: scoreData.username,
+      set: scoreData.set,
+      score: scoreData.score,
+      success: true
+    });
+  } catch (err) {
+    res.status(503).json({ error: err.message });
+  }
+});
+
 export default router;
