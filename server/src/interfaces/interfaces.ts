9c3f0fa73f05e9b58df13ceb4a0e9731ee2b969b
sanqro <contact@sanqro.xyz>
2024-01-02 02:44:11 +0100

    add public attribute to ISetData

--- server/src/interfaces/interfaces.ts
+++ server/src/interfaces/interfaces.ts
@@ -1,20 +1,20 @@
 export interface IAuthFormData {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
 }
 
 export interface ISetData {
-    key: string; // username of creator + set key
-    creator: string;
-    title: string;
-    question: string[];
-    answer: string[];
+  key: string; // username of creator + set key
+  creator: string;
+  title: string;
+  question: string[];
+  answer: string[];
+  public: boolean;
 }
 
-
 export interface IScoreData {
-    key: string; // username + set key
-    username: string;
-    set: string;
-    score: number;
+  key: string; // username + set key
+  username: string;
+  set: string;
+  score: number;
 }
