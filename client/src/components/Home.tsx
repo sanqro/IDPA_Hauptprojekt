eab4a52762f40d4cacc1823df890f7a8dc18b7dc
sanqro <contact@sanqro.xyz>
2024-01-02 01:28:10 +0100

    add check if user is logged in

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,26 +1,47 @@
-// Homepage.tsx
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-const Homepage: React.FC = () => {
-  return (
-    <div className="flex justify-center w-80 align-middle mt-52 rounded-3xl bg-slate-500">
-      <div className="h-72 w-72 flex flex-col justify-center items-center text-white">
-        <img
-          src="https://www.logomaker.com/api/main/images/1j+ojlxEOMkX9Wyqfhe43D6kh...CCrRRPmhzFwXs1M3EMoAJtlikkj...Zt9vo...PExevg9C3ktKMcs8"
-          alt="Logo"
-          className="mb-8"
-        />
+const Homepage = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
 
-        <div className="flex space-x-4">
-          <Link
-            to="/login"
-            className="bg-blue-500 text-white px-4 py-2 rounded-lg hvr-sweep-to-right-login"
-          >
-            Login
-          </Link>
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const username = localStorage.getItem("username") as string;
+    if (
+      username !== null ||
+      token !== null ||
+      username !== undefined ||
+      token !== undefined
+    ) {
+      setUsername(username);
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  return (
+    <div>
+      {isLoggedIn ? (
+        <div className="text-white">{username} </div>
+      ) : (
+        <div className="flex justify-center w-80 align-middle mt-52 rounded-3xl bg-slate-500">
+          <div className="h-72 w-72 flex flex-col justify-center items-center text-white">
+            <img
+              src="https://www.logomaker.com/api/main/images/1j+ojlxEOMkX9Wyqfhe43D6kh...CCrRRPmhzFwXs1M3EMoAJtlikkj...Zt9vo...PExevg9C3ktKMcs8"
+              alt="Logo"
+              className="mb-8"
+            />
+            <div className="flex space-x-4">
+              <Link
+                to="/login"
+                className="bg-blue-500 text-white px-4 py-2 rounded-lg hvr-sweep-to-right-login"
+              >
+                Login
+              </Link>
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
