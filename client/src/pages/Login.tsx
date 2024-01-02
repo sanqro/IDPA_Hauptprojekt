9a79b5260f4bf2eae13527f1594f7192762b8f8f
sanqro <contact@sanqro.xyz>
2024-01-02 01:04:01 +0100

    fix css issue

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -4,7 +4,7 @@ import LoginField from "../components/LoginField";
 
 const Login: React.FC = () => {
   return (
-    <div className="h-72 w-72 flex flex-col justify-center items-center text-white">
+    <div className="h-96 w-72 flex flex-col justify-center items-center text-white">
       <LoginField />
     </div>
   );
