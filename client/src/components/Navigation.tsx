1d7cd415c741e706a6d23c024b1fa429ff360671
sanqro <contact@sanqro.xyz>
2024-01-02 01:51:16 +0100

    add logout button

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,19 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const Navigation: React.FC = () => {
+const Navigation = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
+  const nav = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const storedUsername = localStorage.getItem("username");
+    if (token && storedUsername) {
+      setUsername(storedUsername);
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("username");
+    setUsername("");
+    setIsLoggedIn(false);
+    nav("/login");
+  };
+
   return (
-    <nav className="bg-gray-800 text-white text-center py-4">
-      <ul className="flex justify-center space-x-4">
-        <li className="text-lg font-semibold transition duration-300 hvr-sweep-to-right">
-          <a href="/">Home</a>
-        </li>
-        <li className="text-lg font-semibold transition duration-300 hvr-sweep-to-right">
-          <a href="/cards">Cards</a>
-        </li>
-        <li className="text-lg font-semibold transition duration-300 hvr-sweep-to-right">
-          <a href="/login">Login</a>
-        </li>
-      </ul>
+    <nav className="bg-gray-800 text-white py-4">
+      <div className="flex justify-between items-center px-4">
+        {isLoggedIn && (
+          <div className="text-lg font-semibold">
+            <span>{username}</span>
+          </div>
+        )}
+        <ul
+          className={`flex justify-center space-x-4 ${
+            isLoggedIn ? "flex-grow" : ""
+          }`}
+        >
+          <li className="text-lg font-semibold">
+            <button onClick={() => nav("/")}>Home</button>
+          </li>
+          <li className="text-lg font-semibold">
+            <button onClick={() => nav("/cards")}>Cards</button>
+          </li>
+          {isLoggedIn ? (
+            <li className="text-lg font-semibold transition">
+              <button onClick={handleLogout}>Logout</button>
+            </li>
+          ) : (
+            <li className="text-lg font-semibold transition">
+              <button onClick={() => nav("/login")}>Login</button>
+            </li>
+          )}
+        </ul>
+      </div>
     </nav>
   );
 };
