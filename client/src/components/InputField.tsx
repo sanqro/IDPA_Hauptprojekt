e7015eb2e015035d954fe7701f89a83f93070182
sanqro <contact@sanqro.xyz>
2024-01-03 21:54:52 +0100

    add inputfield component

---
+++ client/src/components/InputField.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { InputField } from "../interfaces/props";
+
+const InputField = (props: InputField) => {
+  return (
+    <input
+      type="text"
+      placeholder={props.placeholder}
+      onChange={props.onChange}
+      className="block w-full px-4 py-2 mt-3 bg-white border"
+    />
+  );
+};
+
+export default InputField;
