6af0e8a18187cd660ec813ed39e1516090a74e1a
sanqro <contact@sanqro.xyz>
2024-01-04 00:17:38 +0100

    add styling to SmallSetDisplay

--- client/src/components/SmallSetDisplay.tsx
+++ client/src/components/SmallSetDisplay.tsx
@@ -9,10 +9,10 @@ const SmallSetDisplay: React.FC<ISmallSetDisplay> = ({
   onClick,
 }) => {
   return (
-    <div>
-      <h2>{set.title}</h2>
-      <p>Type: {set.type || 0}</p>
-      <p>Creator: {set.creator}</p>
+    <div className="bg-gray-400 rounded px-4 pt-4 pb-4 mb-4">
+      <h2 className="mb-1 text-xl font-bold">{set.title}</h2>
+      <p className="mb-1 text-base">Type: {set.type || 0}</p>
+      <p className="mb-1 text-base">Creator: {set.creator}</p>
       <OnClickButton onClick={onClick} label={label} className={className} />
     </div>
   );
