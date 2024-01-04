51d04d76c687d8d5255c2d60e212fc938f16fe21
sanqro <contact@sanqro.xyz>
2024-01-04 01:45:16 +0100

    add missing attribute

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -5,6 +5,7 @@ const InputField = (props: InputField) => {
   return (
     <input
       type="text"
+      value={props.value}
       placeholder={props.placeholder}
       onChange={props.onChange}
       className="block w-full px-4 py-2 mt-3 bg-white border"
