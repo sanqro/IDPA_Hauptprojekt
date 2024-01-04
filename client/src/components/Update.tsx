77c7f9c06aeabb6ab419431a87df68175e62e4b8
sanqro <contact@sanqro.xyz>
2024-01-04 01:46:41 +0100

    add update components

---
+++ client/src/components/Update.tsx
@@ -0,0 +1,115 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useState, useEffect } from "react";
+import { ISet } from "../interfaces/props";
+import SmallSetDisplay from "./SmallSetDisplay";
+import UpdateView from "./UpdateView";
+
+const Update = () => {
+  const [set, setSet] = useState<ISet[]>([]);
+  const [selectedSet, setSelectedSet] = useState<ISet | null>(null);
+
+  useEffect(() => {
+    const fetchSets = async () => {
+      const user = localStorage.getItem("username");
+      const response = await fetch(
+        `https://api.quiz.sanqro.me/sets/get/${user}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: localStorage.getItem("token") as string,
+          },
+        }
+      );
+      const data = await response.json();
+      setSet(data.fetchedSets.items);
+    };
+    fetchSets();
+  }, []);
+
+  const handleEditClick = async (id: string) => {
+    const response = await fetch(
+      `https://api.quiz.sanqro.me/sets/getSet/${id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: localStorage.getItem("token") as string,
+        },
+      }
+    );
+    const data = await response.json();
+    setSelectedSet(data.fetchedSet);
+  };
+
+  const handleSaveClick = async () => {
+    if (!selectedSet) return;
+
+    const oldKey = selectedSet.key;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { key, ...newSetData } = selectedSet;
+
+    try {
+      const response = await fetch("https://api.quiz.sanqro.me/sets/update", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: localStorage.getItem("token") as string,
+        },
+        body: JSON.stringify({ oldKey, ...newSetData }),
+      });
+      if (response.ok) {
+        alert("Set updated successfully");
+        setSelectedSet(null);
+        setSet((prevSets) =>
+          prevSets.map((set) =>
+            set.key === oldKey ? { key: oldKey, ...newSetData } : set
+          )
+        );
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handlePairChange = (type: keyof ISet, index: number, value: string) => {
+    if (selectedSet) {
+      const updatedSet: ISet = { ...selectedSet };
+      (updatedSet[type] as string[])[index] = value;
+      setSelectedSet(updatedSet);
+    }
+  };
+
+  const handleChange = (type: keyof ISet, value: string) => {
+    if (selectedSet && typeof selectedSet[type] === "string") {
+      const updatedSet: ISet = { ...selectedSet };
+      (updatedSet[type] as any) = value;
+      setSelectedSet(updatedSet);
+    }
+  };
+
+  return (
+    <div className="p-4">
+      {selectedSet === null &&
+        set.map((set) => (
+          <SmallSetDisplay
+            key={set.key}
+            set={set}
+            onClick={() => handleEditClick(set.key)}
+            label={"Edit"}
+          />
+        ))}
+      {selectedSet && (
+        <UpdateView
+          selectedSet={selectedSet}
+          handlePairChange={handlePairChange}
+          handleSaveClick={handleSaveClick}
+          handleChange={handleChange}
+          handleCancelClick={() => setSelectedSet(null)}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Update;
---
+++ client/src/components/UpdateView.tsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { IUpdateView } from "../interfaces/props";
+import InputField from "./InputField";
+import OnClickButton from "./OnClickButton";
+
+const EditVocabSetForm = ({
+  selectedSet,
+  handlePairChange,
+  handleSaveClick,
+  handleCancelClick,
+  handleChange,
+}: IUpdateView) => {
+  const [publicToggle, setPublicToggle] = useState(selectedSet.public);
+  return (
+    <div>
+      <InputField
+        type="text"
+        placeholder="Title"
+        value={selectedSet.title}
+        onChange={(e) => handleChange("title", e.target.value)}
+      />
+
+      <select
+        onChange={(e) => handleChange("type", e.target.value)}
+        className="block w-full px-4 py-2 mt-3 bg-white border border-gray-300 rounded-md focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+      >
+        <option value="accounting">Accounting</option>
+        <option value="questions">Questions</option>
+      </select>
+
+      <button
+        onClick={() => setPublicToggle(!publicToggle)}
+        className={`px-4 py-2 text-sm font-semibold mt-2 text-white rounded-md transition-colors ${
+          publicToggle
+            ? "bg-green-500 hover:bg-green-600"
+            : "bg-red-500 hover:bg-red-600"
+        }`}
+      >
+        {publicToggle ? "Public" : "Private"}
+      </button>
+
+      {selectedSet.answer.map((answer, index) => (
+        <div key={index} className="flex space-x-4 mt-4">
+          <InputField
+            type="text"
+            placeholder="Question"
+            value={selectedSet.answer[index]}
+            onChange={(e) => handlePairChange("answer", index, e.target.value)}
+          />
+          <InputField
+            type="text"
+            placeholder="Answer"
+            value={selectedSet.question[index]}
+            onChange={(e) =>
+              handlePairChange("question", index, e.target.value)
+            }
+          />
+        </div>
+      ))}
+      <div>
+        <OnClickButton
+          onClick={() => handleSaveClick()}
+          label="Save"
+          className="py-2 px-4 mt-4 mr-5 text-white bg-blue-500 hover:bg-blue-600 rounded"
+        />
+        <OnClickButton
+          onClick={() => handleCancelClick()}
+          label="Cancel"
+          className="py-2 px-4 mt-4 text-white bg-red-500 hover:bg-red-600 rounded"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default EditVocabSetForm;
