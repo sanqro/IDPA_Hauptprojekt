2a6d9eda563798f26c8d6d52ed067ab0f5c7164a
sanqro <contact@sanqro.xyz>
2024-01-05 22:51:38 +0100

    add scores page to router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,12 +11,13 @@ import UpdateSet from "./pages/UpdateSet.tsx";
 import AuthProvider from "./contexts/AuthProvier.tsx";
 import ProtectedRoutes from "./components/ProtectedRoutes.tsx";
 import Quiz from "./pages/Quiz.tsx";
+import Scores from "./pages/Scores.tsx";
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-        <div className="h-screen w-screen bg-slate-700 rounded">
+        <div className="h-screen w-screen overflow-y-scroll bg-slate-700">
           <Navigation />
           <div className="flex justify-center align-middle">
             <Routes>
@@ -28,6 +29,7 @@ function App() {
                 <Route path="/delete" element={<DeleteSet />} />
                 <Route path="/update" element={<UpdateSet />} />
                 <Route path="/quiz" element={<Quiz />} />
+                <Route path="/scores" element={<Scores />} />
               </Route>
               <Route path="/" element={<Home />} />
               <Route path="*" element={<NotFound />} />
