ba3f6ed2cf3486a007415c9d9d6eb10264ee6388
sanqro <contact@sanqro.xyz>
2024-01-05 21:53:24 +0100

    add feature for multiple types of questions
    Co-authored-by: RelxOff <doeme.kreisel@gmail.com>

--- client/src/components/AddSetInput.tsx
+++ client/src/components/AddSetInput.tsx
@@ -1,26 +1,20 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useState } from "react";
-import { ISetData } from "../interfaces/props"; // Pfad zu Ihrem Interface
-import OnClickButton from "./OnClickButton";
+import { ISet, IQuestionItem, QuestionType } from "../interfaces/props";
 import InputField from "./InputField";
+import OnClickButton from "./OnClickButton";
 
-const addSet = async (set: ISetData) => {
+const addSet = async (set: ISet) => {
   try {
+    set.creator = localStorage.getItem("username") as string;
     const response = await fetch("https://api.quiz.sanqro.me/sets/create", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Authorization: localStorage.getItem("token") as string,
       },
-      body: JSON.stringify({
-        title: set.title,
-        creator: localStorage.getItem("username") as string,
-        question: set.question,
-        answer: set.answer,
-        public: set.public,
-        type: set.type,
-      }),
+      body: JSON.stringify(set),
     });
+
     if (response.ok) {
       alert("Set created successfully");
     } else {
@@ -28,40 +22,52 @@ const addSet = async (set: ISetData) => {
       alert(data.error);
     }
   } catch (error) {
-    alert("Unknown error occured");
+    alert("Unknown error occurred");
   }
 };
 
-function checkInput(set: ISetData) {
-  let questionBoolean = false;
-  let answerBoolean = false;
-  for (let i = 0; i < set.question.length; i++) {
-    if (set.question[i] === "" || set.question[i] === " ") {
-      questionBoolean = true;
-    }
+function checkInput(set: ISet) {
+  if (
+    set.creator === "" ||
+    set.title === "" ||
+    set.type === "" ||
+    !set.question ||
+    set.public === null ||
+    (set.type !== "accounting" && set.type !== "questions")
+  ) {
+    alert("Invalid Input");
+    return false;
   }
-  for (let i = 0; i < set.answer.length; i++) {
-    if (set.answer[i] === "" || set.answer[i] === " ") {
-      answerBoolean = true;
-    }
-  }
-
-  if (set.title === "" || set.title === " ") {
-    alert("Please add a title to your set");
-  } else if (set.question.length === 0 || set.answer.length === 0) {
-    alert("Please add at least one question and one answer");
-  } else if (questionBoolean || answerBoolean) {
-    alert("There is a pair which is empty");
-  } else if (answerBoolean == false && questionBoolean == false) {
-    addSet(set);
-  }
+  return true;
 }
 
-const InputPair = () => {
+const AddSetInput = () => {
+  const handleQuestionTypeChange = (
+    index: number,
+    questionType: QuestionType
+  ) => {
+    const newPairs = [...pairs];
+    newPairs[index].questionType = questionType;
+    if (questionType === QuestionType.MultipleChoice) {
+      newPairs[index].options = [
+        "Option 1",
+        "Option 2",
+        "Option 3",
+        "Option 4",
+      ];
+      newPairs[index].correctAnswer = "Option 1";
+    } else if (questionType === QuestionType.TrueFalse) {
+      newPairs[index].correctAnswer = true;
+    } else {
+      newPairs[index].correctAnswer = "";
+    }
+    setPairs(newPairs);
+  };
+
   const [publicToggle, setPublicToggle] = useState(false);
   const [type, setType] = useState("accounting");
   const [title, setTitle] = useState("");
-  const [pairs, setPairs] = useState<ISetData[]>([]);
+  const [pairs, setPairs] = useState<IQuestionItem[]>([]);
 
   useEffect(() => {}, [type]);
 
@@ -69,40 +75,135 @@ const InputPair = () => {
     setPairs([
       ...pairs,
       {
-        creator: "",
-        title: "",
-        question: [""],
-        answer: [""],
-        public: false,
-        type: "",
+        question: "",
+        questionType: QuestionType.TextInput,
+        correctAnswer: "",
       },
     ]);
   };
 
   const handleChange = (
     index: number,
-    field: keyof ISetData,
-    value: string
+    field: keyof IQuestionItem,
+    value: string | boolean
   ) => {
     const newPairs = [...pairs];
-    if (field === "question" || field === "answer") {
-      (newPairs[index][field] as string[])[0] = value;
-    } else {
-      newPairs[index][field] = value as never;
+    if (field === "questionType") {
+      newPairs[index].questionType = value as QuestionType;
+    } else if (field === "correctAnswer") {
+      newPairs[index].correctAnswer = value as string | boolean;
+    } else if (field === "options" && typeof value === "string") {
+      newPairs[index].options = value.split(",");
+    } else if (field === "question") {
+      newPairs[index].question = value as string;
     }
     setPairs(newPairs);
   };
 
   const handleCreateSet = () => {
-    const Set: ISetData = {
+    const newSet: ISet = {
+      key: (sessionStorage.getItem("username") as string) + title,
       creator: sessionStorage.getItem("username") as string,
       title: title,
-      question: pairs.map((pair) => pair.question[0]),
-      answer: pairs.map((pair) => pair.answer[0]),
+      question: pairs,
       public: publicToggle,
       type: type,
     };
-    checkInput(Set);
+
+    if (checkInput(newSet)) {
+      addSet(newSet);
+    }
+  };
+
+  const handleOptionChange = (
+    pairIndex: number,
+    optionIndex: number,
+    value: string
+  ) => {
+    const newPairs = [...pairs];
+    const currentPair = newPairs[pairIndex];
+
+    if (
+      currentPair.questionType === QuestionType.MultipleChoice &&
+      currentPair.options
+    ) {
+      currentPair.options[optionIndex] = value;
+      setPairs(newPairs);
+    }
+  };
+
+  const handleCorrectAnswerChange = (pairIndex: number, value: string) => {
+    const newPairs = [...pairs];
+    const currentPair = newPairs[pairIndex];
+
+    if (currentPair.questionType === QuestionType.MultipleChoice) {
+      currentPair.correctAnswer = value;
+      setPairs(newPairs);
+    }
+  };
+
+  const displayQuestions = (pair: IQuestionItem, index: number) => {
+    return (
+      <div>
+        <InputField
+          type="text"
+          placeholder="Enter your question here"
+          value={pair.question}
+          onChange={(e) => handleChange(index, "question", e.target.value)}
+        />
+        {pair.questionType === QuestionType.MultipleChoice && (
+          <div>
+            <h3 className="font-semibold mt-2 text-white">Correct answer</h3>
+            <select
+              value={pair.correctAnswer as string}
+              className="block w-full px-4 py-2 mt-3 bg-white border border-gray-300 rounded-md focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+              onChange={(e) => handleCorrectAnswerChange(index, e.target.value)}
+            >
+              {pair.options?.map((option, optionIndex) => (
+                <option key={optionIndex} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+            {pair.options?.map((option, optionIndex) => (
+              <InputField
+                key={optionIndex}
+                type="text"
+                placeholder={`Option ${optionIndex + 1}`}
+                value={option}
+                onChange={(e) =>
+                  handleOptionChange(index, optionIndex, e.target.value)
+                }
+              />
+            ))}
+          </div>
+        )}
+        {pair.questionType === QuestionType.TrueFalse && (
+          <div>
+            <select
+              value={String(pair.correctAnswer)}
+              className="block w-full px-4 py-2 mt-3 bg-white border border-gray-300 rounded-md focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+              onChange={(e) =>
+                handleChange(index, "correctAnswer", e.target.value === "true")
+              }
+            >
+              <option value="true">True</option>
+              <option value="false">False</option>
+            </select>
+          </div>
+        )}
+        {pair.questionType === QuestionType.TextInput && (
+          <InputField
+            type="text"
+            placeholder="Correct Answer"
+            value={pair.correctAnswer as string}
+            onChange={(e) =>
+              handleChange(index, "correctAnswer", e.target.value)
+            }
+          />
+        )}
+      </div>
+    );
   };
 
   return (
@@ -114,9 +215,7 @@ const InputPair = () => {
         value={title}
         onChange={(e) => setTitle(e.target.value)}
       />
-      <h3 className="text-2xl font-semibold mt-2 text-white">
-        Visbility of set:
-      </h3>
+      <h3 className="font-semibold mt-2 text-white">Visbility of set:</h3>
       <button
         onClick={() => setPublicToggle(!publicToggle)}
         className={`px-4 py-2 text-sm font-semibold mt-2 text-white rounded-md transition-colors ${
@@ -127,30 +226,32 @@ const InputPair = () => {
       >
         {publicToggle ? "Public" : "Private"}
       </button>
+      <h3 className="font-semibold mt-2 text-white">Type of set:</h3>
       <select
         onChange={(e) => {
           const newType = e.target.value;
           setType(newType);
         }}
-        className="block w-full px-4 py-2 mt-3 bg-white border border-gray-300 rounded-md focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+        className="block w-full px-4 py-2 mt-2 bg-white border border-gray-300 rounded-md focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
       >
         <option value="accounting">Accounting</option>
         <option value="questions">Questions</option>
       </select>
+      <h3 className="font-semibold mt-2 text-white">Type of question:</h3>
       {pairs.map((pair, index) => (
-        <div className="flex space-x-4 mt-4" key={index}>
-          <InputField
-            type="text"
-            placeholder="Question"
-            value={pair.question[0]}
-            onChange={(e) => handleChange(index, "question", e.target.value)}
-          />
-          <InputField
-            type="text"
-            placeholder="Answer"
-            value={pair.answer[0]}
-            onChange={(e) => handleChange(index, "answer", e.target.value)}
-          />
+        <div className="flex flex-col space-y-4 mt-2" key={index}>
+          <select
+            className="block w-full px-4 py-2 bg-white border border-gray-300 rounded-md focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+            value={pair.questionType}
+            onChange={(e) =>
+              handleQuestionTypeChange(index, e.target.value as QuestionType)
+            }
+          >
+            <option value={QuestionType.TextInput}>Text Input</option>
+            <option value={QuestionType.MultipleChoice}>Multiple Choice</option>
+            <option value={QuestionType.TrueFalse}>True/False</option>
+          </select>
+          {displayQuestions(pair, index)}
         </div>
       ))}
 
@@ -167,5 +268,4 @@ const InputPair = () => {
     </div>
   );
 };
-
-export default InputPair;
+export default AddSetInput;
