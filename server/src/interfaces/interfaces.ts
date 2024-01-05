0e11823468628bdc86a78df0fa403d782f0bc47a
sanqro <contact@sanqro.xyz>
2024-01-05 01:25:34 +0100

    update interface to allow multiple question types

--- server/src/interfaces/interfaces.ts
+++ server/src/interfaces/interfaces.ts
@@ -7,12 +7,23 @@ export interface ISetData {
   key: string; // username of creator + set key
   creator: string;
   title: string;
-  question: string[];
-  answer: string[];
+  question: IQuestionItem[];
   public: boolean;
   type: string;
 }
 
+export interface IQuestionItem {
+  questionType: QuestionType;
+  options?: string[];
+  correctAnswer: string | boolean;
+  userResponse?: string | boolean;
+}
+
+enum QuestionType {
+  MultipleChoice = "multipleChoice",
+  TrueFalse = "trueFalse",
+  TextInput = "textInput"
+}
 export interface IScoreData {
   key: string; // username + set key
   username: string;
